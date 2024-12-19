db5ec5bc6ddab3cf10229e644acb105ca2831b2a
Howard3897613 <112939670+Howard3897613@users.noreply.github.com>
2024-12-19 22:55:04 +0800

    初具雛形

--- src/enum/api.ts
+++ src/enum/api.ts
@@ -2,5 +2,5 @@ export enum api{
     findAll = "http://127.0.0.1:2083/api/v1/user/findAll",
     create ="http://127.0.0.1:2083/api/v1/user/insertOne",
     delete ="http://127.0.0.1:2083/api/v1/user/deleteById",
-    Patch = "http://127.0.0.1:2083/api/v1/user/updateNameById"
+    update = "http://127.0.0.1:2083/api/v1/user/updateNameById"
 }
